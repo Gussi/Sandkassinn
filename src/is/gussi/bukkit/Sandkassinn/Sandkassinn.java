8d708473e9527657ac2c4ae8fed8b381d42a3561
Gussi <gussi@gussi.is>
2012-04-14 13:18:36 +0000

    Implemented vault service provider
    Also some fixes and tweaks

--- src/is/gussi/bukkit/Sandkassinn/Sandkassinn.java
+++ src/is/gussi/bukkit/Sandkassinn/Sandkassinn.java
@@ -2,29 +2,58 @@ package is.gussi.bukkit.Sandkassinn;
 
 import java.util.logging.Logger;
 
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Sandkassinn extends JavaPlugin {
+	// Basic stuff
 	public static Sandkassinn plugin;
-	public static final SandkassinnLogger log = new SandkassinnLogger();
-
-	@Override
-	public void onDisable() {
-		Sandkassinn.log.info(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " is enabled");
-	}
+	public static Log log;
+	
+	// Service providers
+	public static Permission perms;
+	public static Economy econ;
+	public static Chat chat;
 
 	@Override
 	public void onEnable() {
 		Sandkassinn.plugin = this;
+		Sandkassinn.log = new Log(this);
+		
+		// Check if Vault is there
+		if(getServer().getPluginManager().getPlugin("Vault") == null) {
+			Sandkassinn.log.severe("Vault plugin not found, disabling");
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+
+		// Get service providers, care less if it's enabled or not
+		Sandkassinn.perms = getServer().getServicesManager().getRegistration(Permission.class).getProvider();
+		Sandkassinn.econ = getServer().getServicesManager().getRegistration(Economy.class).getProvider();
+		Sandkassinn.chat = getServer().getServicesManager().getRegistration(Chat.class).getProvider();
+
+		Sandkassinn.log.info(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " is enabled");
+	}
+
+	@Override
+	public void onDisable() {
 		Sandkassinn.log.info(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " is disabled");
 	}
 
 	/**
-	 * Static logger class
+	 * Logger class
 	 */
-	public static class SandkassinnLogger {
-		private final Logger log = Logger.getLogger("is.gussi.bukkit.plugin.sandkassinn");
-		protected String prefix = "[GateKeeper] ";
+	public class Log {
+		private Logger log;
+		protected String prefix;
+
+		public Log(JavaPlugin plugin) {
+			this.prefix = "[" + plugin.getDescription().getName() + "] ";
+			this.log = Logger.getLogger("Minecraft.plugin." + plugin.getDescription().getName());
+		}
 
 		public void info(String msg) {
 			this.log.info(this.prefix + msg);

fa360bec8474269a397c9aba62e382e55af749ea
Gussi <gussi@gussi.is>
2012-04-22 12:52:30 +0000

    Common class, having broadcast and sendmessage

---
+++ src/java/main/is/gussi/bukkit/plugin/sandkassinn/Common.java
@@ -0,0 +1,49 @@
+package is.gussi.bukkit.plugin.sandkassinn;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Common methods shared between modules
+ *
+ * @author Gussi
+ */
+public class Common {
+	public static void sendMessage(CommandSender sender, String message) {
+		Common.sendMessage(sender, message, new HashMap<String,String>());
+	}
+
+	public static void sendMessage(CommandSender sender, String message, HashMap<String, String> tokens) {
+		sender.sendMessage(buildString(message, tokens));
+	}
+
+	public static void broadcastMessage(String message) {
+		Common.broadcastMessage(message, new HashMap<String, String>());
+	}
+
+	public static void broadcastMessage(String message, HashMap<String, String> tokens) {
+		Bukkit.getServer().broadcastMessage(buildString(message, tokens));
+	}
+
+	private static String buildString(String message, HashMap<String, String> tokens) {
+		Pattern pattern = Pattern.compile("\\{(.+?)\\}");
+		Matcher matcher = pattern.matcher(message);
+		StringBuilder builder = new StringBuilder();
+		int i = 0;
+		while (matcher.find()) {
+		    String replacement = tokens.get(matcher.group(1));
+		    builder.append(message.substring(i, matcher.start()));
+		    if (replacement == null)
+		        builder.append(matcher.group(0));
+		    else
+		        builder.append(replacement);
+		    i = matcher.end();
+		}
+		builder.append(message.substring(i, message.length()));
+		return builder.toString().replaceAll("&", new Character((char) 167).toString());
+	}
+}
